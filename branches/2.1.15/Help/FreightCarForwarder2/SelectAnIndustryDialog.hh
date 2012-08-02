8bbbc1c7cd3e6671bf6dee8299529edbefbcfaea
heller <heller@3dacd22d-6840-0410-813e-ee3e0c1567db>
2012-08-02 16:25:23 +0000

    git-svn-id: http://svnlocal.deepsoft.com/repos/svnroot/Deepwoods-Public/ModelRRSystem@1164 3dacd22d-6840-0410-813e-ee3e0c1567db

---
+++ branches/2.1.15/Help/FreightCarForwarder2/SelectAnIndustryDialog.hh
@@ -0,0 +1,5 @@
+0 SelectAnIndustryDialog
+This dialog selects an industry to show or report on.  A glob-type
+pattern can be entered in the Industry Name Pattern entry and a list of
+matching industry names will be listed in the list.  An industry can
+then be selected from this list.
