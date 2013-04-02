d1b9d48b47f349787357f3770addd749e50db570
heller <heller@3dacd22d-6840-0410-813e-ee3e0c1567db>
2013-04-02 01:05:22 +0000

    Release 2.1.32
    git-svn-id: http://svnlocal.deepsoft.com/repos/svnroot/Deepwoods-Public/ModelRRSystem@1496 3dacd22d-6840-0410-813e-ee3e0c1567db

---
+++ tags/2.1.32/Scripts/ControlSupport/CmriSupport.tcl
@@ -0,0 +1,267 @@
+#* 
+#* ------------------------------------------------------------------
+#* CmriSupport.tcl - CMRI Node Control Support
+#* Created by Robert Heller on Sun May 30 19:37:31 2004
+#* ------------------------------------------------------------------
+#* Modification History: $Log$
+#* Modification History: Revision 1.2  2007/04/19 17:23:24  heller
+#* Modification History: April 19 Lock Down
+#* Modification History:
+#* Modification History: Revision 1.1  2004/06/06 13:59:45  heller
+#* Modification History: Start of C/MRI Support code.
+#* Modification History:
+#* Modification History: Revision 1.1  2002/07/28 14:03:50  heller
+#* Modification History: Add it copyright notice headers
+#* Modification History:
+#* ------------------------------------------------------------------
+#* Contents:
+#* ------------------------------------------------------------------
+#*  
+#*     Model RR System, Version 2
+#*     Copyright (C) 1994,1995,2002  Robert Heller D/B/A Deepwoods Software
+#* 			51 Locke Hill Road
+#* 			Wendell, MA 01379-9728
+#* 
+#*     This program is free software; you can redistribute it and/or modify
+#*     it under the terms of the GNU General Public License as published by
+#*     the Free Software Foundation; either version 2 of the License, or
+#*     (at your option) any later version.
+#* 
+#*     This program is distributed in the hope that it will be useful,
+#*     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#*     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#*     GNU General Public License for more details.
+#* 
+#*     You should have received a copy of the GNU General Public License
+#*     along with this program; if not, write to the Free Software
+#*     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#* 
+#*  
+#* 
+
+package require snit
+package require cmri 2.0.0
+
+## @defgroup CmriSupport Cmri Support code
+#  @{
+
+namespace eval CmriSupport {
+## @brief Cmri Support code.
+#
+#  This is high-level code to support the CMR/I code, in the form of a SNIT
+#  type object that wraps the low-level class and creates a network of boards
+#  on the bus that the low-level class accesses.
+#
+#  @author Robert Heller @<heller\@deepsoft.com@>
+#
+#  @section CmriSupport_package Package provided
+#
+#  CmriSupport 1.1
+#
+
+
+  snit::type CmriNode {
+  ## @brief CMR/I node type.  
+  #
+  # This Snit type defines CMR/I nodes (SUSIC, USIC, or SMINI
+  # boards) on a CMR/I network.  All options are readonly.
+  #
+  # @param name Name of the node.
+  # @param ... Options:
+  # @arg -type The type of node, one of SUSIC, USIC, or SMINI. No default 
+  #		   value.
+  # @arg -address The address of the node.  Default is 0.
+  # @arg -cardmap The card type map.  Only used with SUSIC and USIC. 
+  #			Default is {}.
+  # @arg -yellowmap The yellow bi-color LED map.  Only used with the SMINI
+  #			card type. Default is {0 0 0 0 0 0}.
+  # @arg -numberofyellow The number of yellow bi-color LED signals. Only
+  #			for SMINI cards.  Default is 0.
+  # @arg -inputports The number of 8-bit input ports.  Default 0 (3 for 
+  #			SMINI cards).
+  # @arg -outputports The number of 8-bit output ports.  Default 0 (6 for 
+  #			SMINI cards).
+  # @arg -delay The delay value to use.  Only meaningful for older (USIC)
+  #			cards.  Default is 0.
+  # @par
+  # @author Robert Heller @<heller\@deepsoft.com@>
+
+
+    typevariable _TypeCodes -array {
+	SUSIC SUSIC
+	USIC  USIC
+	SMINI SMINI
+    }
+    ## @private Node type codes.
+
+    method _ValidateType {option value} {
+    ## @private Method to validate the card type.
+    # @param option The option to validate.
+    # @param value The value to validate.
+
+      if {[lsearch -exact [array names _TypeCodes] [string toupper "$value"]] < 0} {
+	error "Expected a valid card type for $option, got $value"
+      }
+      return "$value"
+    }
+    option -type -readonly yes -validatemethod _ValidateType
+    method _ValidateAddress {option value} {
+    ## @private Method to validate a card address.
+    # @param option The option to validate.
+    # @param value The value to validate.
+
+      if {[string is integer -strict "$value"]} {
+	if {$value >= 0 && $value <= 127} {
+	  return $value
+	} else {
+	  error "Board address for $option out of range (0..127): $value"
+	}
+      } else {
+	error "Expected a board address for $option, got $value"
+      }
+    }
+    option -address -default 0 -readonly yes -validatemethod _ValidateAddress
+    method _ValidateListOfBytes {option value} {
+    ## @private Method to validate a list of bytes.
+    # @param option The option to validate.
+    # @param value The value to validate.
+
+      foreach e $value {
+	if {[string is integer -strict "$e"] &&
+	    $e >= 0 && $e <= 255} {continue}
+	error "Expected a list of bytes for $option, got $value"
+      }
+    }
+    option -cardmap -default {} -readonly yes -validatemethod _ValidateListOfBytes
+    method _ValidateSixElementListOfBytes {option value} {
+    ## @private Method to validate a six element list of bytes.
+    # @param option The option to validate.
+    # @param value The value to validate.
+
+      $self _ValidateListOfBytes $option $value
+      if {[llength $value] != 6} {
+	error error "Expected a 6 element list of bytes for $option, got $value"
+      }
+      return $value
+    }
+    option -yellowmap -default {0 0 0 0 0 0} -readonly yes -validatemethod _ValidateSixElementListOfBytes
+    method _ValidateByte {option value} {
+    ## @private Method to validate a byte value.
+    # @param option The option to validate.
+    # @param value The value to validate.
+
+      if {[string is integer -strict "$value"]} {
+	if {$value >= 0 && $value <= 255} {
+	  return $value
+	} else {
+	  error "Byte value for $option out of range (0..255): $value"
+	}
+      } else {
+	error "Expected a byte for $option, got $value"
+      }
+    }
+    method _ValidateWord {option value} {
+    ## @private Method to validate a word (16-bit) value.
+    # @param option The option to validate.
+    # @param value The value to validate.
+
+      if {[string is integer -strict "$value"]} {
+	if {$value >= 0 && $value <= 65535} {
+	  return $value
+	} else {
+	  error "Word value for $option out of range (0..65535): $value"
+	}
+      } else {
+	error "Expected a 16-bit integer for $option, got $value"
+      }
+    }
+    option -numberofyellow -readonly yes -default 0 -validatemethod _ValidateByte
+    option -inputports -readonly yes -validatemethod _ValidateByte
+    option -outputports -readonly yes -validatemethod _ValidateByte
+    option -delay -readonly yes -default 0 -validatemethod _ValidateWord
+    typecomponent _cmriPort
+    # @private Holds the open CMR/I port object.
+    typemethod openport {{port "/dev/ttyS0"} {baud 9600} {maxtries 10000}} {
+    ## Open the CMR/I port.  This type method opens the CMR/I port.
+    # @param port The serial port connected to the CMR/I network.
+    # @param baud The BAUD rate to be used.
+    # @param maxtries The maximum number of retries.
+
+      if {![catch {set _cmriPort} oldport]} {
+	error "The port is already open ($oldport),  close it first!"
+      }
+      set _cmriPort [CMri "$port" -baud $baud -maxtries $maxtries]
+    }
+    typemethod closeport {} {
+    ## Close the CMR/I port.  This type method closes the CMR/I port.
+
+      if {[catch {set _cmriPort} oldport]} {return}
+      $oldport destroy
+      unset _cmriPort
+    }
+    typemethod portopenp {} {
+    ## Return port status.
+
+      if {[catch {set _cmriPort} oldport]} {
+	return no
+      } else {
+	return yes
+      }
+    }
+    constructor {args} {
+    ## Constructor -- initialize a board.
+    # @param ... Option list.
+
+      if {![$type portopenp]} {
+	error "Port is not open!  Open the port before initializing boards!"
+      }
+      $self configurelist $args
+      switch -exact [string toupper $options(-type)] {
+	SUSIC -
+	USIC {
+	  if {[string length "$options(-inputports)"] == 0} {
+	    set options(-inputports) 0
+	  }
+	  if {[string length "$options(-outputports)"] == 0} {
+	    set options(-outputports) 0
+	  }
+	  $_cmriPort InitBoard "$options(-cardmap)" $options(-inputports) \
+		$options(-outputports) 0 $options(-address) \
+		$_TypeCodes([string toupper $options(-type)]) \
+		$options(-delay)
+	}
+	SMINI {
+	  if {[string length "$options(-inputports)"] == 0} {
+	    set options(-inputports) 3
+	  }
+	  if {[string length "$options(-outputports)"] == 0} {
+	    set options(-outputports) 6
+	  }
+	  $_cmriPort InitBoard "$options(-yellowmap)" $options(-inputports) \
+		$options(-outputports) $options(-numberofyellow) \
+		$options(-address) \
+		$_TypeCodes([string toupper $options(-type)]) 0
+	}
+      }
+    }
+    method inputs {} {
+    ## Method to fetch input port values.
+
+      return [$_cmriPort Inputs $options(-inputports) $options(-address)]
+    }
+    method outputs {portvector} {
+    ## Method to set output ports.
+    # @param portvector Vector of output ports.
+
+      $self _ValidateListOfBytes portvector "$portvector"
+      if {[llength "$portvector"] != $options(-outputports)} {
+	error "Wrong number of output port values, should be $options(-outputports), got $portvector ([llength $$portvector] elements)."
+      }
+      return [$_cmriPort Outputs $portvector $options(-address)]
+    }
+  }
+}
+
+## @}
+    
+package provide CmriSupport 1.1
---
+++ tags/2.1.32/Scripts/Dispatcher/WrapIt.tcl
@@ -0,0 +1,185 @@
+#* 
+#* ------------------------------------------------------------------
+#* WrapIt.tcl - Code to wrap a dispatcher file into a starpack.
+#* Created by Robert Heller on Thu Jan 27 17:57:03 2011
+#* ------------------------------------------------------------------
+#* Modification History: $Log$
+#* Modification History: Revision 1.1  2002/07/28 14:03:50  heller
+#* Modification History: Add it copyright notice headers
+#* Modification History:
+#* ------------------------------------------------------------------
+#* Contents:
+#* ------------------------------------------------------------------
+#*  
+#*     Model RR System, Version 2
+#*     Copyright (C) 2011  Robert Heller D/B/A Deepwoods Software
+#* 			51 Locke Hill Road
+#* 			Wendell, MA 01379-9728
+#* 
+#*     This program is free software; you can redistribute it and/or modify
+#*     it under the terms of the GNU General Public License as published by
+#*     the Free Software Foundation; either version 2 of the License, or
+#*     (at your option) any later version.
+#* 
+#*     This program is distributed in the hope that it will be useful,
+#*     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#*     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#*     GNU General Public License for more details.
+#* 
+#*     You should have received a copy of the GNU General Public License
+#*     along with this program; if not, write to the Free Software
+#*     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#* 
+#*  
+#* 
+
+package require Mk4tcl
+
+namespace eval WrapIt {
+  variable TclKit {}
+  variable HasTested no
+  variable PackageBaseDir {}
+  proc islink {file} {
+    set result no
+    catch {
+	file lstat $file stat
+	if {$stat(type) eq "link"} {set result yes}
+    }
+    return $result
+  }
+  proc SetTclkit {kit} {
+    variable TclKit
+    variable HasTested
+    if {$kit ne {}} {
+      set tclkit $kit
+      while {[file exists $tclkit] && [islink $tclkit]} {
+	set tclkit [file readlink $tclkit]
+      }
+      if {[file exists $tclkit] && [file readable $tclkit]} {
+	set TclKit $tclkit
+      }
+      set HasTested yes
+    }
+  }
+  proc CheckPackageBaseDir {} {
+    variable PackageBaseDir
+    puts stderr "*** WrapIt::CheckPackageBaseDir (at start): PackageBaseDir = '$PackageBaseDir'"
+    if {$PackageBaseDir ne {}} {return}
+    set bindir [file dirname [info nameofexecutable]]
+    puts stderr "*** WrapIt::CheckPackageBaseDir: bindir = $bindir"
+    set instdir [file dirname $bindir]
+    puts stderr "*** WrapIt::CheckPackageBaseDir: instdir = $instdir"
+    if {[file isdirectory [file join $instdir XPressNet]] &&
+	[file isdirectory [file join $instdir RailDriverSupport]] &&
+	[file isdirectory [file join $instdir NCE]]} {
+      # Running from Build dir
+      set PackageBaseDir $instdir
+    } elseif {[file isdirectory [file join $instdir share]] &&
+	      [file isdirectory [file join $instdir share MRRSystem]]} {
+      # Running from Install dir
+      set PackageBaseDir [file join $instdir share MRRSystem]
+    }
+    puts stderr "*** WrapIt::CheckPackageBaseDir (after if): PackageBaseDir = '$PackageBaseDir'"
+  }
+  proc CanWrapP {} {
+    variable TclKit
+    variable HasTested
+    if {$HasTested} {return [expr {$TclKit ne ""}]}
+    set bindir [file dirname [info nameofexecutable]]
+    set tclkit [file join $bindir tclkit[file extension [info nameofexecutable]]]
+    while {[file exists $tclkit] && [islink $tclkit]} {
+      set tclkit [file readlink $tclkit]
+    }
+#    puts stderr "*** WrapIt::CanWrapP: bindir = $bindir, tclkit = $tclkit"
+    if {[file exists $tclkit] && [file readable $tclkit]} {
+      set TclKit $tclkit
+    }
+    set HasTested yes
+    return [expr {$TclKit ne ""}]
+  }
+  variable Lib [file join [file dirname [file dirname [file dirname [info script]]]] lib]
+#  puts stderr "*** WrapIt::Lib = $Lib"
+  variable CodeLibrary [file join [file dirname [file dirname [file dirname [info script]]]] CodeLibrary]
+#  puts stderr "*** WrapIt::CodeLibrary = $CodeLibrary"
+  variable CopyLibDirs [list \
+    [glob -nocomplain [file join $Lib bwidget*]] \
+    [glob -nocomplain [file join $Lib snit*]] \
+  ]
+#  puts stderr "*** WrapIt::CopyLibDirs = $CopyLibDirs"
+  variable CMriLibDir [glob -nocomplain [file join $CodeLibrary CMri]]
+#  puts stderr "*** WrapIt::CMriLibDir = $CMriLibDir"
+  variable AzatraxLibDir [glob -nocomplain [file join $CodeLibrary Azatrax]]
+#  puts stderr "*** WrapIt::AzatraxLibDir = $AzatraxLibDir"
+  variable CopyCommonLibFiles [list \
+    [file join $Lib Common BWStdMenuBar.tcl] \
+    [file join $Lib Common mainwindow.tcl] \
+    [file join $Lib Common CTCPanel2.tcl] \
+    [file join $Lib Common grsupport2.tcl] \
+    [file join $Lib Common panedw.tcl] \
+    [file join $Lib Common gettext.tcl] \
+  ]
+#  puts stderr "*** WrapIt::CopyCommonLibFiles = $CopyCommonLibFiles"
+  proc WrapIt {filename writeprogfun {needcmri no} {needazatrax no} {additionalPackages {}}} {
+    variable TclKit
+    variable PackageBaseDir
+    set compress 1
+    set ropts -readonly
+    file copy $TclKit $filename
+    vfs::mk4::Mount $filename $filename
+    set module [file rootname [file tail $filename]]
+    set fp [open [file join $filename main.tcl] w]
+    puts $fp "
+  package require starkit
+  starkit::startup
+  package require app-$module
+"
+    close $fp
+    variable CopyLibDirs
+    foreach ld $CopyLibDirs {
+#      puts stderr "*** WrapIt::WrapIt: ld = $ld"
+#      puts stderr "*** WrapIt::WrapIt: file copy $ld [file join $filename lib [file tail $ld]]"
+      file copy $ld [file join $filename lib [file tail $ld]]
+    }
+    variable CopyCommonLibFiles
+    file mkdir [file join $filename lib Common]
+    set pkgIndex [file join $filename lib Common pkgIndex.tcl]
+    set pkgSrcFp [open [file join [info nameofexecutable] lib Common pkgIndex.tcl] r]
+    set pkgSrc [read $pkgSrcFp]
+    close $pkgSrcFp
+    set pkgFp [open $pkgIndex w]
+    foreach lf $CopyCommonLibFiles {
+      file copy $lf [file join $filename lib Common]
+      regexp -line "^(package ifneeded .*\\\$dir [file tail $lf].*)\$" $pkgSrc => pkgLine
+#      puts stderr "*** WrapIt::WrapIt: pkgLine = $pkgLine"
+      puts $pkgFp "$pkgLine"
+    }
+    close $pkgFp
+    if {$needcmri} {
+      variable CMriLibDir
+      file copy $CMriLibDir [file join $filename lib [file tail $CMriLibDir]]
+    }
+    if {$needazatrax} {
+      variable AzatraxLibDir
+      file copy $AzatraxLibDir [file join $filename lib [file tail $AzatraxLibDir]]
+    }
+    
+    foreach ap $additionalPackages {
+      file mkdir [file join $filename lib $ap]
+      foreach f [glob -nocomplain [file join $PackageBaseDir $ap *.tcl]] {
+        file copy $f [file join $filename lib $ap]
+      }
+    }      
+    set lib [file join $filename lib]
+    file mkdir [file join $lib app-$module]
+    set fp [open [file join $lib app-$module $module.tcl] w]
+    eval $writeprogfun $fp $module yes
+    close $fp
+    set fp [open [file join $lib app-$module pkgIndex.tcl] w]
+    puts $fp "package ifneeded app-$module 1.0 \[list source \[file join \$dir $module.tcl\]\]"
+    close $fp
+    vfs::unmount $filename
+  }
+}
+
+package provide WrapIt 1.0
+    
