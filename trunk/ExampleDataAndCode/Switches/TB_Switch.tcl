b4d42eecb68587b6f2dfe6887201685f3ebec7d1
heller <heller@3dacd22d-6840-0410-813e-ee3e0c1567db>
2015-08-04 17:31:53 +0000

    Add Train Brain Switch code
    git-svn-id: http://svnlocal.deepsoft.com/repos/svnroot/Deepwoods-Public/ModelRRSystem@2018 3dacd22d-6840-0410-813e-ee3e0c1567db

---
+++ trunk/ExampleDataAndCode/Switches/TB_Switch.tcl
@@ -0,0 +1,58 @@
+#*****************************************************************************
+#
+#  System        : 
+#  Module        : 
+#  Object Name   : $RCSfile$
+#  Revision      : $Revision$
+#  Date          : $Date$
+#  Author        : $Author$
+#  Created By    : Robert Heller
+#  Created       : Sat Aug 1 14:05:17 2015
+#  Last Modified : <150801.1410>
+#
+#  Description	
+#
+#  Notes
+#
+#  History
+#	
+#*****************************************************************************
+#
+#    Copyright (C) 2015  Robert Heller D/B/A Deepwoods Software
+#			51 Locke Hill Road
+#			Wendell, MA 01379-9728
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+# 
+#
+#*****************************************************************************
+
+
+package require CTIAcela;# require the CTIAcela package
+package require snit;#    require the SNIT OO framework
+
+snit::type TB_Switch {
+    ##
+    # @brief Switch (turnout) operation using a CTI Train Brain and Yardmaster
+    #
+    # @image html switch-CTITB-thumb.png
+    # @image latex switch-CTITB.png "Switch controlled by CTI's Yardmaster and Train Brain" width=5in
+    #
+    
+    
+
+}
+
