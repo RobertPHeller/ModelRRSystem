19a7bfc362f1da7f09dabf22201dd8975178012a
heller <heller@3dacd22d-6840-0410-813e-ee3e0c1567db>
2015-08-19 14:39:23 +0000

    MacOSX App Bundle Installer
    git-svn-id: http://svnlocal.deepsoft.com/repos/svnroot/Deepwoods-Public/ModelRRSystem@2082 3dacd22d-6840-0410-813e-ee3e0c1567db

--- trunk/BuildScripts/macappInstall.sh
+++ trunk/BuildScripts/macappInstall.sh
@@ -7,9 +7,12 @@ for bf in `find $1`; do
    if [ -d $bf ] ; then
       echo "install -d $2/$bf"
       install -d $2/$bf
-   else
+   elif [ -x $bf ] ; then
       echo "install $bf $2/$bf"
       install $bf $2/$bf
+   else
+      echo "install $bf $2/$bf"
+      install -m 644 $bf $2/$bf
    fi
 done
   
