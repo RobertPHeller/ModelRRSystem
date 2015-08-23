5182f6e3ad4ca96fc5e9bcba3d1fbaa02dd7abb6
heller <heller@3dacd22d-6840-0410-813e-ee3e0c1567db>
2015-08-23 18:33:50 +0000

    MacOSX Setup Changes
    git-svn-id: http://svnlocal.deepsoft.com/repos/svnroot/Deepwoods-Public/ModelRRSystem@2111 3dacd22d-6840-0410-813e-ee3e0c1567db

--- branches/2.1.36/Installer/setup.tcl
+++ branches/2.1.36/Installer/setup.tcl
@@ -584,17 +584,17 @@ proc Deep_File_Copy {sourceDir destDir logtext} {
   foreach f [glob -nocomplain [file join $sourceDir *]] {
     switch [file type $f] {
       file {
-	file copy $f $destDir
+	catch {file copy $f $destDir}
 	$logtext insert end "[file join $destDir [file tail $f]]\n"
 	$logtext see end
-        update
+        update idle
       }
       directory {
 	set newdir [file join $destDir [file tail $f]]
 	file mkdir $newdir
         $logtext insert end "$newdir\n"
 	$logtext see end
-	update
+	update idle
 	Deep_File_Copy $f $newdir $logtext
       }
     }
