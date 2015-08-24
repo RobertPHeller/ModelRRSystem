00ccb09a9f56d44c638aea29e8e4238c57a0d7b2
heller <heller@3dacd22d-6840-0410-813e-ee3e0c1567db>
2015-08-24 13:25:31 +0000

    Remove old tag
    git-svn-id: http://svnlocal.deepsoft.com/repos/svnroot/Deepwoods-Public/ModelRRSystem@2150 3dacd22d-6840-0410-813e-ee3e0c1567db

--- tags/2.1.7/Help/FreightCarForwarder2/ControlYardListsDialog.hh
+++
@@ -1,4 +0,0 @@
-0 ControlYardListsDialog
-This dialog box controls whether or not Yard Lists are generated or not. 
-You can chose lists sorted alphabetically by car or lists sorted by
-train.  You can also chose to get two copies of either list.
--- tags/2.1.7/Help/FreightCarForwarder2/SelectATrainDialog.hh
+++
@@ -1,4 +0,0 @@
-0 SelectATrainDialog
-This dialog is used to select a train.  A glob type pattern can be
-entered for the train name and matching train names can be listed in the
-list and a specific train can then be selected from this list.
