7b1812a39c111e30d0d3ea6749f91f6049a1e72f
heller <heller@3dacd22d-6840-0410-813e-ee3e0c1567db>
2015-08-24 13:28:03 +0000

    Remove old tag
    git-svn-id: http://svnlocal.deepsoft.com/repos/svnroot/Deepwoods-Public/ModelRRSystem@2161 3dacd22d-6840-0410-813e-ee3e0c1567db

--- tags/2.1.14a/BuildScripts/generateLib_pkgIndex.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd $2
-export LD_LIBRARY_PATH=`pwd`
-echo "pkg_mkIndex -verbose -lazy . $3"|$1
-
-
--- tags/2.1.14a/Help/FreightCarForwarder2/SelectADivisionDialog.hh
+++
@@ -1,5 +0,0 @@
-0 SelectADivisionDialog
-This dialog is used to select a division.  A glob type pattern can be
-entered for the division name and matching division names can be listed in the
-list and a specific division can then be selected from this list.
-
