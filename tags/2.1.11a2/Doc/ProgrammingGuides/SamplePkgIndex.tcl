77bf555e1fac26f0bc7e3619c74a4ef257238a60
heller <heller@3dacd22d-6840-0410-813e-ee3e0c1567db>
2015-08-24 13:27:37 +0000

    Remove old tag
    git-svn-id: http://svnlocal.deepsoft.com/repos/svnroot/Deepwoods-Public/ModelRRSystem@2156 3dacd22d-6840-0410-813e-ee3e0c1567db

--- tags/2.1.11a2/Doc/ProgrammingGuides/SamplePkgIndex.tcl
+++
@@ -1,6 +0,0 @@
-package ifneeded SampleCodeMain 1.0 [list source [file join $dir SampleCodeMain.tcl]]
-package ifneeded SampleCodeCTCPanel 1.0 [list source [file join $dir SampleCodeCTCPanel.tcl]]
-package ifneeded SampleCodeRC 1.0 [list source [file join $dir SampleCodeRC.tcl]]
-#package ifneeded SampleCodeMain 1.0 [list source [file join $dir SampleCodeMain.tcl]]
-#package ifneeded SampleCodeMain 1.0 [list source [file join $dir SampleCodeMain.tcl]]
-
