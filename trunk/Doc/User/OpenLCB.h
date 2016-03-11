f24c193c4c4a4cce5104943e366e6c4764f66161
heller <heller@3dacd22d-6840-0410-813e-ee3e0c1567db>
2016-03-11 19:50:28 +0000

    Build Infrastructure for Open LCB Program user reference
    git-svn-id: http://svnlocal.deepsoft.com/repos/svnroot/Deepwoods-Public/ModelRRSystem@2279 3dacd22d-6840-0410-813e-ee3e0c1567db

---
+++ trunk/Doc/User/OpenLCB.h
@@ -0,0 +1,62 @@
+// -!- c++ -!- //////////////////////////////////////////////////////////////
+//
+//  System        : 
+//  Module        : 
+//  Object Name   : $RCSfile$
+//  Revision      : $Revision$
+//  Date          : $Date$
+//  Author        : $Author$
+//  Created By    : Robert Heller
+//  Created       : Fri Mar 11 14:43:58 2016
+//  Last Modified : <160311.1446>
+//
+//  Description	
+//
+//  Notes
+//
+//  History
+//	
+/////////////////////////////////////////////////////////////////////////////
+//
+//    Copyright (C) 2016  Robert Heller D/B/A Deepwoods Software
+//			51 Locke Hill Road
+//			Wendell, MA 01379-9728
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// 
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef __OPENLCB_H
+#define __OPENLCB_H
+/** @mainpage Table Of Contents
+ * @anchor toc
+ * @htmlonly
+ * <div class="contents">
+ * <div class="textblock"><ol type="1">
+ * <li><a class="el" href="openlcb.html">OpenLCB Reference</a></li>
+ * <li><a class="el" href="help.html">Help</a></li>
+ * <li><a class="el" href="Version.html">Version</a></li>
+ * <li><a class="el" href="Copying.html">Copying</a><ol type="a">
+ * <li><a class="el" href="Copying.html#Warranty">Warranty</a></li>
+ * </ol>
+ * </li>
+ * </ol></div></div>
+ * @endhtmlonly
+ */
+
+#endif // __OPENLCB_H
+
