e101ef8ae53659fd445ce5edb0f96132ada7765c
heller <heller@3dacd22d-6840-0410-813e-ee3e0c1567db>
2016-08-28 16:22:04 +0000

    Add documentation hooks to Daemon configuration screens
    git-svn-id: http://svnlocal.deepsoft.com/repos/svnroot/Deepwoods-Public/ModelRRSystem@2471 3dacd22d-6840-0410-813e-ee3e0c1567db

--- trunk/Scripts/LCC/OpenLCB_MRD2.tcl
+++ trunk/Scripts/LCC/OpenLCB_MRD2.tcl
@@ -8,7 +8,7 @@
 #  Author        : $Author$
 #  Created By    : Robert Heller
 #  Created       : Sun Jun 26 11:43:33 2016
-#  Last Modified : <160817.1546>
+#  Last Modified : <160828.1211>
 #
 #  Description	
 #
@@ -423,7 +423,13 @@ snit::type OpenLCB_MRD2 {
             {command "[_m {Menu|Edit|Cu&t}]" {edit:cut edit:havesel} "[_ {Cut selection to the paste buffer}]" {Ctrl x} -command {StdMenuBar EditCut}}
             {command "[_m {Menu|Edit|&Copy}]" {edit:copy edit:havesel} "[_ {Copy selection to the paste buffer}]" {Ctrl c} -command {StdMenuBar EditCopy}}
             {command "[_m {Menu|Edit|C&lear}]" {edit:clear edit:havesel} "[_ {Clear selection}]" {} -command {StdMenuBar EditClear}}
-        }
+        } "[_m {Menu|&Help}]" {help} {help} 0 {
+            {command "[_m {Menu|Help|On &Help...}]" {help:help} "[_ {Help on help}]" {} -command {HTMLHelp help Help}}
+            {command "[_m {Menu|Help|On &Version}]" {help:help} "[_ {Version}]" {} -command {HTMLHelp help Version}}
+            {command "[_m {Menu|Help|Warranty}]" {help:help} "[_ {Warranty}]" {} -command {HTMLHelp help Warranty}}
+            {command "[_m {Menu|Help|Copying}]" {help:help} "[_ {Copying}]" {} -command {HTMLHelp help Copying}}
+            {command "[_m {Menu|Help|OpenLCB Daemons Reference Manual}]" {help:help} {} {} -command {HTMLHelp help "OpenLCB Daemons"}}
+        } 
     }
     
     # Default (empty) XML Configuration.
@@ -445,6 +451,11 @@ snit::type OpenLCB_MRD2 {
         package require snitStdMenuBar
         package require ButtonBox
         package require ScrollTabNotebook
+        package require HTMLHelp 2.0
+        
+        set HelpDir [file join [file dirname [file dirname [file dirname \
+                                                            [info script]]]] Help]
+        HTMLHelp setDefaults "$HelpDir" "index.html#toc"
         
         set conffilename $conffile
         set confXML $default_confXML
