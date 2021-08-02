bae7fecae927d9eb2360e96ab964ccb9b0f78ec0
heller <heller@3dacd22d-6840-0410-813e-ee3e0c1567db>
2021-08-02 12:36:42 +0000

    Working on EquipmentInventory.
    git-svn-id: http://svnlocal.deepsoft.com/repos/svnroot/Deepwoods-Public/ModelRRSystem@2970 3dacd22d-6840-0410-813e-ee3e0c1567db

---
+++ trunk/Scripts/EquipmentInventory/RollingStock.tcl
@@ -0,0 +1,61 @@
+#*****************************************************************************
+#
+#  System        : 
+#  Module        : 
+#  Object Name   : $RCSfile$
+#  Revision      : $Revision$
+#  Date          : $Date$
+#  Author        : $Author$
+#  Created By    : Robert Heller
+#  Created       : Sun Aug 1 21:43:47 2021
+#  Last Modified : <210802.0835>
+#
+#  Description	
+#
+#  Notes
+#
+#  History
+#	
+#*****************************************************************************
+#
+#    Copyright (C) 2021  Robert Heller D/B/A Deepwoods Software
+#			51 Locke Hill Road
+#			Wendell, MA 01379-9728
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+# 
+#
+#*****************************************************************************
+
+
+package require csv
+package require snit
+
+
+
+
+snit::type RollingStock {
+    variable reportingMarks_
+    variable number_
+    variable type_
+    variable description_
+    variable length_
+    variable clearance_
+    variable weightClass_
+    variable emptyWeight_
+    variable loadedWeight_
+    variable imageFile_
+    variable 
