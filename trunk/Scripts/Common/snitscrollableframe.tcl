0c4752ffae422035700213bf8f539e70f09a943d
heller <heller@3dacd22d-6840-0410-813e-ee3e0c1567db>
2021-08-21 21:41:07 +0000

    Minor bugfix: translation error from BWidget code in Scrollable Frame
    git-svn-id: http://svnlocal.deepsoft.com/repos/svnroot/Deepwoods-Public/ModelRRSystem@2981 3dacd22d-6840-0410-813e-ee3e0c1567db

--- trunk/Scripts/Common/snitscrollableframe.tcl
+++ trunk/Scripts/Common/snitscrollableframe.tcl
@@ -8,7 +8,7 @@
 #  Author        : $Author$
 #  Created By    : Robert Heller
 #  Created       : Thu Jul 11 10:02:32 2013
-#  Last Modified : <160223.1307>
+#  Last Modified : <210821.1544>
 #
 #  Description	
 #
@@ -130,8 +130,8 @@ snit::widgetadaptor ScrollableFrame {
         set y1  [expr {$y0+[winfo height $widget]}]
         set xb0 [$hull canvasx 0]
         set yb0 [$hull canvasy 0]
-        set xb1 [$hull canvasx [winfo width  $path]]
-        set yb1 [$hull canvasy [winfo height $path]]
+        set xb1 [$hull canvasx [winfo width  $widget]]
+        set yb1 [$hull canvasy [winfo height $widget]]
         set dx  0
         set dy  0
         
